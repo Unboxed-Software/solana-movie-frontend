f5cbd0f3c467dde65530fcbbd17ef9530bc8e852
James Pacheco <jamesrp13@gmail.com>
2022-04-23 14:14:23 -0600

    Starter code

---
+++ components/Form.tsx
@@ -0,0 +1,70 @@
+import { FC } from 'react';
+import { Movie } from '../models/Movie';
+import { useState } from 'react';
+import { Box, Button, FormControl, FormLabel, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Textarea } from '@chakra-ui/react';
+
+export const Form: FC = () => {
+    const [title, setTitle] = useState('')
+    const [rating, setRating] = useState(0)
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault()
+        const movie = new Movie(title, rating, message)
+        console.log(JSON.stringify(movie))
+        // submit transaction here
+    }
+
+    return (
+        <Box
+            p={4}
+            display={{ md: "flex" }}
+            maxWidth="32rem"
+            borderWidth={1}
+            margin={2}
+            justifyContent="center"
+        >
+            <form onSubmit={handleSubmit}>
+                <FormControl isRequired>
+                    <FormLabel color='gray.200'>
+                        Movie Title
+                        </FormLabel>
+                    <Input 
+                    id='title' 
+                    color='gray.400'
+                    onChange={event => setTitle(event.currentTarget.value)}
+                />
+                </FormControl>
+                <FormControl isRequired>
+                    <FormLabel color='gray.200'>
+                        Add your review
+                        </FormLabel>
+                    <Textarea 
+                        id='review' 
+                        color='gray.400'
+                        onChange={event => setMessage(event.currentTarget.value)}
+                    />
+                </FormControl>
+                <FormControl isRequired>
+                    <FormLabel color='gray.200'>
+                        Rating
+                        </FormLabel>
+                    <NumberInput 
+                        max={5} 
+                        min={1} 
+                        onChange={(valueString) => setRating(parseInt(valueString))}
+                    >
+                        <NumberInputField id='amount' color='gray.400' />
+                        <NumberInputStepper color='gray.400'>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                        </NumberInputStepper>
+                    </NumberInput>
+                </FormControl>
+                <Button width="full" mt={4} type="submit">
+                    Submit Review
+                </Button>
+            </form>
+        </Box>
+    );
+}
