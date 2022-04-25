759a1f9d10971fc3cd07bc44197fed1d294ab9f7
James Pacheco <jamesrp13@gmail.com>
2022-04-24 22:49:55 -0600

    Fetch and deserialize accounts

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,24 +1,32 @@
 import { Card } from './Card'
 import { FC, useEffect, useState } from 'react'
 import { Movie } from '../models/Movie'
+import * as web3 from '@solana/web3.js'
 
 const MOVIE_REVIEW_PROGRAM_ID = '4X5hVHsHeGHjLEB9hrUqQ57sEPcYPPfW54fndmQrsgCF'
 
 export const MovieList: FC = () => {
+    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
     const [movies, setMovies] = useState<Movie[]>([])
 
     useEffect(() => {
-        setMovies(Movie.mocks)
+        connection.getProgramAccounts(new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID)).then(async (accounts) => {
+            const movies: Movie[] = accounts.reduce((accum: Movie[], { pubkey, account }) => {
+                const movie = Movie.deserialize(account.data)
+                if (!movie) {
+                    return accum
+                }
+
+                return [...accum, movie]
+            }, [])
+            setMovies(movies)
+        })
     }, [])
     
     return (
         <div>
             {
-                movies.map((movie, i) => {
-                    return (
-                        <Card key={i} movie={movie} />
-                    )
-                })
+                movies.map((movie, i) => <Card key={i} movie={movie} /> )
             }
         </div>
     )
--- models/Movie.ts
+++ models/Movie.ts
@@ -25,9 +25,30 @@ export class Movie {
         borsh.str('description'),
     ])
 
+    static borshAccountSchema = borsh.struct([
+        borsh.bool('initialized'),
+        borsh.u8('rating'),
+        borsh.str('title'),
+        borsh.str('description'),
+    ])
+
     serialize(): Buffer {
         const buffer = Buffer.alloc(1000)
         this.borshInstructionSchema.encode({ ...this, variant: 0 }, buffer)
         return buffer.slice(0, this.borshInstructionSchema.getSpan(buffer))
+    }
+
+    static deserialize(buffer?: Buffer): Movie|null {
+        if (!buffer) {
+            return null
+        }
+
+        try {
+            const { title, rating, description } = this.borshAccountSchema.decode(buffer)
+            return new Movie(title, rating, description)
+        } catch(e) {
+            console.log('Deserialization error:', e)
+            return null
+        }
     }
 }
