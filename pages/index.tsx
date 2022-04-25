4736381d223a46fb4df78d754d553cde03f38d68
James Pacheco <jamesrp13@gmail.com>
2022-04-24 21:24:44 -0600

    Simplify index and create MovieList

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,18 @@
 import { Center, Box, Heading } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import { AppBar } from '../components/AppBar'
-import { Card } from '../components/Card'
+import { MovieList } from '../components/MovieList'
 import { Form } from '../components/Form'
-import { Movie } from '../models/Movie'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-
   return (
     <div className={styles.App}>
+      <Head>
+        <title>Movie Reviews</title>
+      </Head>
       <AppBar />
-
       <Center>
         <Box>
           <Heading as="h1" size="l" color="white" ml={4} mt={8}>
@@ -24,13 +23,7 @@ const Home: NextPage = () => {
           <Heading as="h1" size="l" color="white" ml={4} mt={8}>
             Existing Reviews
           </Heading>
-          {
-            Movie.mocks.map((movie, i) => {
-              return (
-                <Card key={i} movie={movie} />
-              )
-            })
-          }
+          <MovieList />
         </Box>
       </Center>
     </div>
