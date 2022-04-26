570961c9c46ad0c6a76758d6378ea35a1689b550
James Pacheco <jamesrp13@gmail.com>
2022-04-26 00:05:38 -0600

    Update program id

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -2,7 +2,7 @@ import { Card } from './Card'
 import { FC, useEffect, useState } from 'react'
 import { Movie } from '../models/Movie'
 
-const MOVIE_REVIEW_PROGRAM_ID = '4X5hVHsHeGHjLEB9hrUqQ57sEPcYPPfW54fndmQrsgCF'
+const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
 
 export const MovieList: FC = () => {
     const [movies, setMovies] = useState<Movie[]>([])
