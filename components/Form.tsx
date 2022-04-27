906dbd2cca5aa02ebfb31a4f894a692bab6a153d
James Pacheco <jamesrp13@gmail.com>
2022-04-27 13:30:19 -0700

    Update programId in `Form`

--- components/Form.tsx
+++ components/Form.tsx
@@ -5,7 +5,7 @@ import { Box, Button, FormControl, FormLabel, Input, NumberDecrementStepper, Num
 import * as web3 from '@solana/web3.js'
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 
-const MOVIE_REVIEW_PROGRAM_ID = '4X5hVHsHeGHjLEB9hrUqQ57sEPcYPPfW54fndmQrsgCF'
+const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
 
 export const Form: FC = () => {
     const [title, setTitle] = useState('')
