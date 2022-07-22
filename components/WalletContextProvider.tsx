a4077dc1c034c6711317d6e5e3142d9a4eba62b3
James Pacheco <jamesrp13@gmail.com>
2022-07-22 08:13:51 -0600

    Add missing import

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -3,6 +3,7 @@ import {
     ConnectionProvider,
     WalletProvider,
 } from "@solana/wallet-adapter-react"
+import * as web3 from "@solana/web3.js"
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
 import * as walletAdapterWallets from "@solana/wallet-adapter-wallets"
 require("@solana/wallet-adapter-react-ui/styles.css")
