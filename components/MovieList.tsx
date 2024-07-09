08b96111dd019b0da38ce020f2af4401d24b0731
sourabh <sourabh7146@gmail.com>
2024-07-09 09:51:08 +0530

    paging and ordering

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,33 +1,54 @@
 import { Card } from './Card'
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect, useMemo, useState } from 'react'
 import { Movie } from '../models/Movie'
 import * as web3 from '@solana/web3.js'
-import { useConnection } from "@solana/wallet-adapter-react"
-
-const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
+import { MovieCoordinator } from '../coordinators/MovieCoordinator'
+import { Button, Center, HStack, Input, Spacer } from '@chakra-ui/react'
 
 export const MovieList: FC = () => {
-  const { connection } = useConnection()
-  const [movies, setMovies] = useState<Movie[]>([])
+    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
+    const [movies, setMovies] = useState<Movie[]>([])
+    const [page, setPage] = useState(1)
+    const [search, setSearch] = useState('')
 
-  useEffect(() => {
-    connection.getParsedAccountInfo
-    connection.getProgramAccounts(new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID)).then(async (accounts) => {
-        const movies: Movie[] = accounts.reduce((acc :Movie[] , { account }) => {
-        console.log(account);
-        const movie = Movie.deserialize(account.data)
-            if (!movie) return acc;
-            return [...acc,movie]
-      },[]);
-      setMovies(movies)
-    })
-  }, [])
-
-  return (
-    <div>
-      {
-        movies.map((movie, i) => <Card key={i} movie={movie} /> )
-      }
-    </div>
-  )
+    useEffect(() => {
+        MovieCoordinator.fetchPage(
+            connection, 
+            page, 
+            5,
+            search,
+            true
+        ).then(setMovies)
+    }, [page, search])
+    
+    return (
+        <div>
+            <Center>
+                <Input
+                    id='search'
+                    color='gray.400'
+                    onChange={event => setSearch(event.currentTarget.value)}
+                    placeholder='Search'
+                    w='97%'
+                    mt={2}
+                    mb={2}
+                />
+            </Center>
+            {
+                movies.map((movie, i) => <Card key={i} movie={movie} /> )
+            }
+            <Center>
+                <HStack w='full' mt={2} mb={8} ml={4} mr={4}>
+                    {
+                        page > 1 && <Button onClick={() => setPage(page - 1)}>Previous</Button>
+                    }
+                    <Spacer />
+                    {
+                        MovieCoordinator.accounts.length > page * 5 &&
+                        <Button onClick={() => setPage(page + 1)}>Next</Button>
+                    }
+                </HStack>
+            </Center>
+        </div>
+    )
 }
